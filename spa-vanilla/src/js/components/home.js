4beac46d9d121788cf507b9c9a87e8546a5fe35d
Gal Parselani <Parselinho@gmail.com>
2023-12-20 18:52:36 +0200

    classes changes

--- spa-vanilla/src/js/components/home.js
+++ spa-vanilla/src/js/components/home.js
@@ -1,15 +1,3 @@
-// const injectHTML = () => {
-//   return `
-
-//     <h1>Home</h1>
-//     <div role="button" data-href="/search" class="button-style"><span>Search</span></div>
-//     <a href="/about">About</a>
-//     <br>
-//     <a href="/about/me">About Me</a>`;
-// };
-
-// export default injectHTML();
-
 class Home {
   constructor(parent) {
     this.parent = parent;
